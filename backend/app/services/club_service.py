a817bb556d81b1acd8e91b791da6f1c525ddb10e
smrive4 <193434163+smrive4@users.noreply.github.com>
2026-04-18 00:31:35 -0500

    Added ClubService methods

--- backend/app/services/club_service.py
+++ backend/app/services/club_service.py
@@ -1,3 +1,51 @@
 
+from app.models.club import Club
+from app.schemas.club import ClubCreate
+from app.db.repositories import DBRepository
+
+
 class ClubService():
-    pass
+    def __init__(self, repo: DBRepository, session):
+        self.repo = repo
+        self.session = session
+
+    def get_all_clubs(self):
+        return self.repo.get_all_clubs(self.session)
+    
+    def get_club(self, club_id):
+        return self.repo.get_club(club_id, self.session)
+    
+    def create_club(self, club:ClubCreate):
+        new_club = Club()
+        new_club.name = club.name
+        new_club.owner_id = club.owner_id
+        new_club.privacy = club.privacy
+
+        self.repo.save_club(new_club, self.session)
+        self.session.refresh(new_club)
+        return new_club
+    
+    def join_club(self, user_id, club_id):
+        user = self.repo.get_user(user_id, self.session)
+        club = self.repo.get_club(club_id, self.session)
+
+        club.members.append(user)
+        self.repo.save_club(club, self.session)
+        self.session.refresh(club)
+        return club
+    
+    def leave_club(self, user_id, club_id):
+        user = self.repo.get_user(user_id, self.session)
+        club = self.repo.get_club(club_id, self.session)
+
+        club.members.remove(user)
+        self.repo.save_club(club)
+        self.session.refresh(club)
+        return club
+    
+    def delete_club(self, club_id):
+        club = self.repo.get_club(club_id, self.session)
+        if club:
+            self.repo.delete_club(club_id, self.session)
+        return club
+        
